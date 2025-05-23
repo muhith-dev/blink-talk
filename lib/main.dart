09ff0b2726de58bd51180bb7588f0e41ec6870c8
yuzuteaaa <abdulmuhithnawawi@gmail.com>
2025-05-23 14:14:44 +0700

    initial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:blink_talk/app/services/auth_service.dart';
 import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
 List<CameraDescription>? cameras;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   cameras = await availableCameras();
 
   final isLoggedIn = await AuthService.isLoggedIn();
